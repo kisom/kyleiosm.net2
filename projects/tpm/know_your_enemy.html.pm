fee95b69b6319ec56b59194f568f08834f1504b7
Kyle Isom <kyle@tyrfingr.is>
2014-07-27 19:58:17 -0700

    Update KYM

--- projects/tpm/know_your_enemy.html.pm
+++ projects/tpm/know_your_enemy.html.pm
@@ -25,4 +25,70 @@ with:}
 
 ◊p{If the system is physically secure, it might make sense to have the
 SRK use the well-known secret so that the machine can perform certain
-activites unattended.}
+activites unattended. Alternatively, a single unlock when the system
+starts to unlock the TPM might be appropriate. The latter typically
+requires human intervention but provides a greater measure of
+security. Note that any program that wishes to use the TPM must unlock
+the TPM.}
+
+◊h3{Scenario 2: Single primary application}
+
+◊p{In this case, the security model is similar to the previous case;
+the application's keys may be protected by an SRK secret, or an
+additional or separate secret for the application's top-level storage
+key(s) should be provided.}
+
+◊h3{Separate applications}
+
+◊p{Here, each application has its own key, and must both unlock the
+SRK and the relevant keys.}
+
+◊h3{Applying the security model}
+
+◊p{Generally, human intervention is preferred such that the secret(s)
+required to unlock the appropriate keys are kept in memory for the
+shortest possible time; perhaps something akin to the following C
+code:}
+
+◊pre{
+        /*
+         * Eliding the particulars of connecting a TPM, it will just
+         * be represented as an opaque structure pointer.
+         */
+        static TPM tpm;
+
+        int
+        unlock_tpm()
+        {
+                uint8_t	secret[MAX_SECRET_LENGTH];
+                size_t  seclen = 0;
+
+                while (1) {
+                      if (-1 == read_password(&secret, &seclen)) {
+                              fprintf(stderr, "Failed to read the password");
+                              return -1;
+                      }
+                      
+                      if (0 == unlock_tpm(secret, seclen)) {
+                              memset(&secret, 0, seclen)
+                              break;
+                      }
+
+                      fprintf(stderr, "Invalid secret.\n");
+                }
+                return 0;
+        }
+}
+
+◊p{As long as the application maintains the TPM handle and barring any
+failures of the TPM or TSS, the application should now be able to
+perform cryptographic operations with the TPM.}
+
+◊p{However, if the threat model is based on a physically secure
+system, the secret might be baked into the program. However, if the
+binary or source is ever leaked, the TPM is no longer
+trustworthy. Furthermore, the security gained by keeping the private
+key out of memory is now lost as an attacker who has access to the box
+will likely be able to recover the secret. This is why I
+◊strong{strongly} prefer human intervention here; the threat model
+should justify deviations.}
