c9dd2b032c3be3aceda8c54e9611c0b1d4ff560c
Kyle Isom <kyle@cloudflare.com>
2014-08-11 15:51:56 -0700

    Add PoC||GTFO stash.
    PDFs not in git because I have to pull this repo back down over 3G.
    Yes, 3G.

---
+++ pocgtfo/index.html.pm
@@ -0,0 +1,37 @@
+#lang pollen
+
+◊h2{POC||GTFO}
+
+◊h3{Самиздат}
+
+◊ul{
+
+  ◊li{2014/08, ◊link["pocorgtfo05.pdf"]{Issue 0x05}: ◊em{PoC||GTFO;
+  addressed to the inhabitants of Earth on the following and other
+  interesting subjects written for the edification of all good
+  neighbors}}
+
+  ◊li{2014/06, ◊link["pocorgtfo04.pdf"]{Issue 0x04}: ◊em{Tract de la
+  Société Secrète de PoC||GTFO sur l’évangile des machines étranges et
+  autres sujets techniques par le prédicateur Pasteur Manul
+  Laphroaig}}
+
+  ◊li{2014/03, ◊link["pocorgtfo03.pdf"]{Issue 0x03}: ◊em{An Address to
+  the Secret Society of PoC||GTFO concerning the Gospel of the Weird
+  Machines and also the Smashing of Idols to Bits and Bytes by the
+  Rt. Revd. Dr. Pastor Manul Laphroaig}}
+
+  ◊li{2013/12, ◊link["pocorgtfo02.pdf"]{Issue 0x02}: ◊em{Children’s
+  Bible Coloring Book of PoC||GTFO Issue 0x02, an Epistle to the 30th
+  CCC Congress in Hamburg}}
+
+  ◊li{2013/10, ◊link["pocorgtfo01.pdf"]{Issue 0x01}: ◊em{An Epistle to
+  the 10th H2HC in São Paulo, From the writing desk, not the raven, of
+  Rt. Revd. Preacherman Pastor Manul Laphroaig}}
+
+  ◊li{2013/08, ◊link["pocorgtfo00.pdf"]{Issue 0x00}: ◊em{An epistle
+  from the desk of Rt. Revd. Pastor Manul Laphroaig}}
+
+}
+
+◊p{◊link["pocorgtfo.SHA256"]{SHA256 sums}}
