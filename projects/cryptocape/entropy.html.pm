f6ad9195cddbff5e9a247af10655400d67e39f8a
Kyle Isom <kyle@cloudflare.com>
2014-09-23 06:53:20 -0700

    Update entropy project page.

--- projects/cryptocape/entropy.html.pm
+++ projects/cryptocape/entropy.html.pm
@@ -8,19 +8,18 @@ VPSes tend to have poor and disfunctional PRNGs. I needed a way to get
 entropy from a higher-quality PRNG to these VPSes, particularly since
 once runs an instance of
 ◊link["https://github.com/cloudflare/cfssl/"]{◊code{cfssl}} that's
-used for generating keys.}
-
-◊p{Fortunately, the CryptoCape has a TPM with a HWRNG, and several
+used for generating keys.
+Fortunately, the CryptoCape has a TPM with a HWRNG, and several
 other fun hardware crypto chips that can be mixed into the operating
-system PRNG.}
+system PRNG, and this can be used to my advantage.}
 
-◊p{I ended up building a system to do this. The VPSes run a server
-program (the "◊strong{sink}") that listens for incoming connections,
-and the BBB runs a d◊|ae|mon (the "◊strong{source}") that periodically
-(every six hours, in my case) sends a packet of random data to the
-VPSes. The BBB has a signing RSA key, which I plan on migrating to a
-TPM signature key later, and each VPS has an RSA key pair to which
-entropy packets are encrypted.}
+◊p{I ended up building a system to use the CryptoCape and a Beaglebone
+to distribute randomness to the VPSes, which run a server program (the
+"◊strong{sink}") that listens for incoming connections; the BBB runs a
+d◊|ae|mon (the "◊strong{source}") that periodically (every six hours)
+sends a packet of random data to the VPSes. The BBB has a signing RSA
+key, which I plan on migrating to a TPM signature key later, and each
+VPS has an RSA key pair to which entropy packets are encrypted.}
 
 ◊p{The randomness packets are defined as}
 
@@ -36,18 +35,34 @@ packet ::= SEQUENCE {
 the packet with its signature key, then encrypts the packet to the
 appropriate sink. Naturally, a new packet is prepared for each sink
 that the source is tracking. A 16-bit unsigned integer indicating the
-length of the packet is written to sink first, followed by the
-encrypted packet. The source ensures the counter hasn't regressed,
-that the timestamp is within an acceptable drift range, and then
-decrypts and verifies the signature on the packet. If all goes well,
-the chunk of randomness is written to the system PRNG.}
+length of the packet is written to the sink first, followed by the
+encrypted packet. The sink decrypts the packet and checks its
+signature, ensures the counter hasn't regressed, and verifies that the
+timestamp is within an acceptable drift range. If all goes well, the
+chunk of randomness is written to the system PRNG.}
 
 ◊p{The source runs a copy of the
 ◊link["https://github.com/gokyle/gofortuna"]{Fortuna} PRNG, stirring
 in entropy occasionally from the system PRNG and the TPM. In the
-future, I'd like to add support for reading data from the ATSHA204.}
+future, I'd like to add a Fortuna source that reads data from the
+ATSHA204 on the cape.}
 
 ◊p{The first proof-of-concept version of this code is
-◊link["https://github.com/kisom/entropysharing"]{available on Github}.}
+◊link["https://github.com/kisom/entropysharing"]{available on Github}.
+This proof of concept works, but there are some improvements
+planned:}
+
+◊ul{
+
+  ◊li{The packets should be encrypted with NaCl.}
+
+  ◊li{The sinks should use TLS-protected HTTPS endpoints with client
+  authentication.}
+
+  ◊li{The TPM should be used for packet signatures.}
+
+  ◊li{The ATSHA204 should be used as another source of entropy.}
+
+}
 
 ◊p{◊small{Back to ◊link["index.html"]{CryptoCape projects}.}}
