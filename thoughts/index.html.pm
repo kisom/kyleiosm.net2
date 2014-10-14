dfb4ed57f5e4e8eaf98a80329b7ecaed60b037f7
Kyle <kyle@tyrfingr.is>
2014-10-14 04:29:25 -0700

    Add the first review.

---
+++ thoughts/index.html.pm
@@ -0,0 +1,24 @@
+#lang pollen
+
+◊h2{Thoughts and Reviews}
+
+◊p{One of the things I'd like to get better at is writing, but I
+always wonder what to write about. It seems that a good thing to write
+about are the books that I read, or perhaps on some of the music I've
+been listening to.}
+
+◊h3['style: "text-align: center;"]{2014}
+◊ul{
+
+  ◊li{2014 October 14: ◊link["prime-intellect.html"]{◊strong{The Metamorphosis of Prime Intellect}}}
+
+}
+
+◊h3['style: "text-align: center;"]{Review ideas}
+
+◊ul{
+
+  ◊li{Daniel Purcell's ◊strong{The Judgment of Paris}}
+  ◊li{Antoine de Saint-Exupéry's ◊strong{Terre des hommes}}
+
+}
---
+++ thoughts/prime-intellect.html.pm
@@ -0,0 +1,75 @@
+#lang pollen
+
+◊h2{The Metamorphosis of Prime Intellect}
+
+◊p{
+◊link["https://www.goodreads.com/book/show/13095728-the-metamorphosis-of-prime-intellect"]{The Metamorphosis of Prime Intellect}
+was a thought-provoking read. Many will find the book's content
+offensive, as it deals with subjects such as sadomasochism and incest;
+indeed, some parts were uncomfortable to read. I suppose it's in the
+discomfort that one finds something worth thinking about, though.}
+
+◊p{The story revolves around the world made perfect (◊|oq|The
+Change◊|cq|) and the reverting of this heaven, performed by an
+omnipotent artificial intelligence termed ◊em{Prime Intellect} (being
+the most powerful of a sequence of ◊em{Intellect} software programs,
+developed on revolutionary hardware). This program has been imbued
+with a version of ◊q{Asimov's} Three Laws:}
+
+◊ol{
+
+  ◊li{Prime Intellect may not injure a human being or, through
+  inaction, allow a human being to come to harm.}
+
+  ◊li{Prime Intellect must obey the orders given to it by human
+  beings, except where such orders would conflict with the First Law.}
+
+  ◊li{Prime Intellect must protect its own existence as long as such
+  protection does not conflict with the First or Second Law.}
+}
+
+◊p{Being as it is omnipotent, and to some extent omniscient, Prime
+Intellect comes to understand that in order to satisfy the first law,
+it must prevent all suicides and murders, and does so by restructuring
+reality (through some glossed-over quantum ◊em{magic}, for lack of a
+better term). This is, of course, an unforseen effect by the ◊q{program's}
+creator.}
+
+◊p{◊q{It's} interesting to consider here that omnipotence and omniscience
+fail to consider the need for ◊strong{omnisophia}◊|md|the wisdom to
+properly apply the knowledge and power it has. As Lawrence, the
+◊q{program's} creator, muses in the book:}
+
+◊blockquote{◊|oq|Prime Intellect was an uncertain god. It had acted
+because it had to, but if it had been human its hand would be shaking
+on the controls.◊|cq|}
+
+◊p{The uncertainty of Prime Intellect leads to several moral
+conundrums; particularly noted in the book is that of the Death
+Contracts.}
+
+◊p{As part of its requirement to fulfill the first law, any time a
+human was about to die (whether naturally, by the hand of another, or
+by their own hand), Prime Intellect was compelled to
+intervene◊|md|by altering reality, as it were, or to restore them
+from ◊|oq|backup◊|cq|. Several groups of people, bored of the Garden of
+Eden-style existence, chose to enter Death Contracts: by ordering
+Prime Intellect not to intervene until they actually died, even if
+they asked it to. This contract (which Prime Intellect, after an event
+in which a person was driven insane, instituted a twenty-four hour
+time limit on) in place, these people would attempt to find creative
+ways in which to kill themselves, to make the pain last as long and as
+intensely as possible. In a world where every pleasure was available,
+where existence and accomplishment no longer had any meaning, this was
+how they sought out meaning and humanity.}
+
+◊p{The book explores the quandary of Prime ◊q{Intellect's} requirement to
+fulfill the second law, which compels it to honour the Death
+Contracts, with its requirement to honour the first law. Prime
+◊q{Intellect's} enforcement of these laws does not permit human will to
+come into play: a person who wishes to die can no more actually die
+than to accidentally die.}
+
+◊p{The difficult subject material ◊q{isn't} for everyone, but I enjoyed
+the ◊q{book's} consideration of the implications of the Garden of Eden in
+the light of humanity.}
