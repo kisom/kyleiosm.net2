83b96c53bd5924a5b48680af7b88ab2b45f37251
Kyle <kyle@tyrfingr.is>
2014-12-29 19:51:31 -0700

    Add packages, start writing up docs for crypto.

---
+++ packages/index.html.pm
@@ -0,0 +1,22 @@
+#lang pollen
+
+◊h2{Packages}
+
+◊p{This is a place to host packages for some of the projects I work
+on.}
+
+◊ul{
+
+  ◊li{◊link["/packages/cfssl/"]{cfssl}}
+
+  ◊li{◊link["/packages/entropyshare/"]{entropyshare} (Linux/amd64 only)}
+
+}
+
+◊h3{See also}
+
+◊p{
+
+  ◊li{◊link["/projects/cryptutils/"]{cryptutils}}
+
+}
